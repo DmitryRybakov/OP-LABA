fb97aaebae8d94415535bd1cd6e1326ce218631c
DmitryRybakov <90505662+DmitryRybakov@users.noreply.github.com>
2021-12-01 15:14:46 +0300

    Add files via upload

---
+++ Laba 6/C++/Laba 6 c++.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int SUM(int sum, int a);
+
+int main()
+{
+	setlocale(LC_ALL, "Ukrainian");
+
+	cout << "Введiть тiльки перше довiльне натуральне n-значне число: ";
+	int k;
+	cin >> k;
+	int sum, max = 0, a, x = 0;
+	while (k != 0) {
+		sum = 0;
+		a = k;
+		sum = SUM(sum, a);
+		if (max < sum) {
+			max = sum;
+			x = k;
+		}
+		cout << "Число сума цифр якого є найбільшою - " << x << " Сума цифр цього числа дорівнює " << max << endl;
+		cout << "Введiть наступне довiльне натуральне n-значне число, або введiть 0 для завершення программи: ";
+		cin >> k;
+	}
+	return 0;
+}
+
+int SUM(int sum, int a) {
+	for (sum; a != 0; sum) {
+		sum += a % 10;
+		a /= 10;
+	}
+	return sum;
+}
+
